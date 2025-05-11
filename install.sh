1453203b740e3f49722da163e89364cfc13ea77e
Nino <ninzgt@gmail.com>
2025-05-11 12:57:31 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
+clear
+echo -e "${GREEN}Installing Proxy...${ENDCOLOR}"
+sleep 1
+if [ -f "proxy" ]; then
+    echo -e "${RED}Deleting old proxy...${ENDCOLOR}"
+    rm proxy
+    sleep 1
+    echo -e "${GREEN}Getting proxy...${ENDCOLOR}"
+fi
+wget -q https://github.com/ninostn/proxy/raw/main/noxproxy
+sleep 1
+echo -e "${GREEN}NOX PROXY is now Installed.${ENDCOLOR}"
+echo -e "${RED}PROXY BY @ninostn${ENDCOLOR}"
+echo -e "${GREEN}Execute proxy with this command: ./noxproxy${ENDCOLOR}"
+chmod +x noxproxy
